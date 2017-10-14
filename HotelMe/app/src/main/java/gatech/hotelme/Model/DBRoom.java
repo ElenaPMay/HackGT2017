bf8f2e0e04b61da341aab1f02fa2f8f79444cdd2
Caroline Kish <ckish3@gatech.edu>
2017-10-14 07:49:34 -0400

    added database handler classes and database functionality

---
+++ HotelMe/app/src/main/java/gatech/hotelme/Model/DBRoom.java
@@ -0,0 +1,54 @@
+package gatech.hotelme.Model;
+
+
+public class DBRoom {
+    private int _roomNumber;
+    private String _hotelID;
+    private String _roomType;
+    private int _reservationID;
+
+
+    public int get_roomNumber() {
+        return _roomNumber;
+    }
+
+    public void set_roomNumber(int _roomNumber) {
+        this._roomNumber = _roomNumber;
+    }
+
+    public String get_hotelID() {
+        return _hotelID;
+    }
+
+    public void set_hotelID(String _hotelID) {
+        this._hotelID = _hotelID;
+    }
+
+    public String get_roomType() {
+        return _roomType;
+    }
+
+    public void set_roomType(String roomType) {
+        this._roomType = roomType;
+    }
+
+    public int get_reservationID() {
+        return _reservationID;
+    }
+
+    public void set_reservationID(int _reservationID) {
+        this._reservationID = _reservationID;
+    }
+
+    DBRoom() {
+
+    }
+
+    public DBRoom(int _roomNumber, String _hotelID, String roomType,
+                  int _reservationID) {
+        this._roomNumber = _roomNumber;
+        this._hotelID = _hotelID;
+        this._roomType = roomType;
+        this._reservationID = _reservationID;
+    }
+}
