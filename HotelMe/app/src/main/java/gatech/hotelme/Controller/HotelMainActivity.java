a76ce9cbc2b9c0b4cf2a3da64a8216d1cd2776ed
rbanerjee34 <ruchi.banerjee@gatech.edu>
2017-10-14 09:12:03 -0400

    Did controller for login, check in screen, home screen, and key screen

--- HotelMe/app/src/main/java/gatech/hotelme/Controller/HotelMainActivity.java
+++ HotelMe/app/src/main/java/gatech/hotelme/Controller/HotelMainActivity.java
@@ -1,5 +1,6 @@
 package gatech.hotelme.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +16,7 @@ public class HotelMainActivity extends AppCompatActivity {
     }
 
     public void onGetKeyPressed(View view) {
-
+        startActivity(new Intent(getBaseContext(),HotelKeyActivity.class));
+        finish();
     }
 }
