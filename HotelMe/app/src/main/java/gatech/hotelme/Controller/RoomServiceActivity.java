42d4f6ec7af1eb33fbc0b6a870e8888271a341a5
Caroline Kish <ckish3@gatech.edu>
2017-10-15 08:22:05 -0400

    final changes

--- HotelMe/app/src/main/java/gatech/hotelme/Controller/RoomServiceActivity.java
+++ HotelMe/app/src/main/java/gatech/hotelme/Controller/RoomServiceActivity.java
@@ -1,12 +1,15 @@
 package gatech.hotelme.Controller;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import gatech.hotelme.R;
@@ -22,8 +25,22 @@ public class RoomServiceActivity extends AppCompatActivity {
         menuListView = (ListView) findViewById(R.id.menu);
         String[] string_array = {"food item 1", "food item 2", "food item 3",
                 "food item 4", "food item 5"};
-        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout
-                .support_simple_spinner_dropdown_item, string_array);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout
+                .support_simple_spinner_dropdown_item, string_array) {
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup
+                    parent) {
+                View view =super.getView(position, convertView, parent);
+                TextView textView = (TextView) view.findViewById(android.R.id
+                        .text1);
+
+                textView.setTextColor(Color.WHITE);
+
+                return view;
+            }
+        };
         menuListView.setAdapter(adapter);
     }
 
